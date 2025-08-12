b367b2b7f55e4997fa6a4a5bb10bfbcacb1a9a0f
MohitKandpal <mohit.mcs17.du@gmail.com>
2025-08-12 17:55:08 +0530

    Adding solution for Invert Tree problem
    Leetcode: - https://leetcode.com/problems/invert-binary-tree/?envType=problem-list-v2&envId=oizxjoit

---
+++ src/com/dsa/InvertBinaryTree.java
@@ -0,0 +1,26 @@
+/**
+ * Given the root of a binary tree, invert the tree, and return its root.
+ */
+package com.dsa;
+
+public class InvertBinaryTree {
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(3);
+        root.left = new TreeNode(4);
+        root.right = new TreeNode(5);
+        root.left.left = new TreeNode(1);
+        root.left.right = new TreeNode(2);
+
+        TreeNode newRoot = invertTree(root);
+    }
+
+    public static TreeNode invertTree(TreeNode root) {
+        if(root == null)
+            return null;
+        TreeNode left = root.left;
+        TreeNode right = root.right;
+        root.right = left;
+        root.left = right;
+        return root;
+    }
+}

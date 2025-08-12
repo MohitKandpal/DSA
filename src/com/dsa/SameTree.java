ac57fb9b3ea6051ce58610b5aae14835f1d1b8c1
MohitKandpal <mohit.mcs17.du@gmail.com>
2025-08-12 17:50:44 +0530

    Adding solution for Same Tree problem
    Leetcode: - https://leetcode.com/problems/same-tree/?envType=problem-list-v2&envId=oizxjoit

---
+++ src/com/dsa/SameTree.java
@@ -0,0 +1,31 @@
+/**
+ * Given the roots of two binary trees p and q, write a function to check if they are the same or not.
+ * Two binary trees are considered the same if they are structurally identical, and the nodes have the same value.
+ */
+package com.dsa;
+
+public class SameTree {
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(3);
+        root.left = new TreeNode(4);
+        root.right = new TreeNode(5);
+        root.left.left = new TreeNode(1);
+        root.left.right = new TreeNode(2);
+
+        TreeNode root2 = new TreeNode(3);
+        root2.left = new TreeNode(4);
+        root2.right = new TreeNode(5);
+        root2.left.left = new TreeNode(1);
+        root2.left.right = new TreeNode(2);
+
+        System.out.println("Trees identical: - " + sameTree(root, root2));
+    }
+
+    public static boolean sameTree(TreeNode root1, TreeNode root2) {
+        if(root1 == null && root2 == null)
+            return true;
+        if(root1 == null || root2 == null)
+            return false;
+        return root1.val == root2.val && sameTree(root1.left, root2.left) && sameTree(root1.right, root2.right);
+    }
+}
